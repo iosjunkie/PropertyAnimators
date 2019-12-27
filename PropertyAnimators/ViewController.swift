e83974528a4b5fe769b84b5f30bca49ba91a6fce
Jules Lee <jackconsolidated@gmail.com>
2019-12-27 22:35:21 +0800

    Allow, start, pause, stop, reverse, completion, and addAnimation on the fly

--- PropertyAnimators/ViewController.swift
+++ PropertyAnimators/ViewController.swift
@@ -27,6 +27,10 @@ import UIKit
 class ViewController: UIViewController {
     var animator: UIViewPropertyAnimator!
     @IBOutlet weak var block: UIView!
+    @IBOutlet weak var startButton: UIButton!
+    @IBOutlet weak var pauseButton: UIButton!
+    @IBOutlet weak var stopButton: UIButton!
+    @IBOutlet weak var slider: UISlider!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -35,16 +39,59 @@ class ViewController: UIViewController {
     }
 
     func setupAnimation() {
+        self.block.transform = .identity
+        self.block.frame.origin.x = 0
+        
         animator = UIViewPropertyAnimator(duration: 1.2, curve: .linear, animations: {
             let screenWidth = self.view.frame.size.width
             let blockWidth = self.block.frame.size.width
             
             self.block.frame.origin.x = screenWidth - blockWidth
+            self.block.transform = CGAffineTransform(rotationAngle: CGFloat.pi)
         })
     }
     
     @IBAction func start(_ sender: UIButton) {
-        animator.startAnimation()
+        if animator.isRunning {
+            return
+        }
+        
+        switch animator.state {
+        case .active:
+            animator.isReversed = true
+            animator.addAnimations {
+                self.block.transform = CGAffineTransform(scaleX: 1.7, y: 1.7)
+            }
+            
+            animator.startAnimation()
+        case .inactive:
+            setupAnimation()
+            animator.startAnimation()
+            break
+        case .stopped:
+            animator.finishAnimation(at: .end)
+            
+        @unknown default:
+            fatalError()
+        }
+        
+        animator.addCompletion { (pos) in
+            self.block.backgroundColor = .red
+        }
+    }
+    
+    @IBAction func pause(_ sender: Any) {
+        if animator.state == .active {
+            animator.pauseAnimation()
+        }
+    }
+    
+    @IBAction func stop(_ sender: Any) {
+        animator.stopAnimation(true)
+    }
+    
+    @IBAction func sliderDidChange(_ sender: Any) {
+        animator.fractionComplete = CGFloat(slider.value) // only available when animation is paused
     }
 }
 
